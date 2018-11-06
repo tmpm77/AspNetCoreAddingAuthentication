e0df7342637c2af5be7c781dd62d6278fc0f1a4d
tmpm77 <tmalone7@gmail.com>
2018-11-06 12:04:44 -0600

    Completed the module

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Models;
+using WishList.Models.AccountViewModels;
 
 namespace WishList.Controllers
 {
@@ -15,6 +17,37 @@ namespace WishList.Controllers
         {
             _userManager = userManager;
             _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(viewModel);
+
+            var result = await _userManager.CreateAsync(new ApplicationUser
+            {
+                UserName = viewModel.Email,
+                Email = viewModel.Email
+            }, viewModel.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError("Password", error.Description);
+
+                return View(viewModel);
+            }
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
